61487d730ecb2a24a0057a816742aeda8039a962
Luiz <valuiz1@gmail.com>
2022-12-21 16:11:24 -0300

    Created simple views and templates. Currently dealing with an issue where django can't find static files, remember to solve that.

--- tournament_website/urls.py
+++ tournament_website/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    # path('addfighter/'),
+    path('', views.index),
+    path('addfighter/', views.addfighter),
 ]
--- tournament_website/views.py
+++ tournament_website/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, )
+    return render(request, "index.html")
+
+def addfighter(request):
+    return render(request, "addfighter.html")
